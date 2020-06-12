ff607284eb32862bd2452dd68e17b1bb226a307f
Jeremy Glebe <jeremyglebe@gmail.com>
2020-06-11 23:04:06 -0500

    changed console instantiation to be simpler

--- src/client/scenes/TestBox.ts
+++ src/client/scenes/TestBox.ts
@@ -1,5 +1,4 @@
 import { Console } from '../tools/Console';
-import { px, py } from '../tools/PercentCoords';
 
 export class TestBox extends Phaser.Scene {
     create() {
@@ -8,30 +7,7 @@ export class TestBox extends Phaser.Scene {
 
     createConsole() {
         let gameConsole = new Console(this);
-
-        let container: HTMLDivElement = document.createElement('div');
-        container.id = 'log-container';
-        container.style.overflow = 'auto';
-        container.style.width = '300px';
-        container.style.height = '150px';
-        container.style.backgroundColor = '#222';
-
-        let highlighting: HTMLStyleElement = document.createElement('style');
-        highlighting.type = 'text/css';
-        highlighting.innerHTML = ".log-warn { color: orange } \
-        .log-error { color: red } \
-        .log-info { color: skyblue } \
-        .log-log { color: silver } \
-        .log-warn, .log-error { font-weight: bold; }";
-        container.appendChild(highlighting);
-
-        let outputText: HTMLPreElement = document.createElement('pre');
-        outputText.id = 'log';
-        container.appendChild(outputText);
-
-        this.add.dom(px(100) - 150, py(100) - 75, container);
-
-        gameConsole.rewireAll(outputText, container);
+        gameConsole.rewireAll();
 
         for (let i = 0; i < 100; i++) {
             console.log(i);
--- src/client/tools/Console.ts
+++ src/client/tools/Console.ts
@@ -1,3 +1,5 @@
+import { px, py } from "./PercentCoords";
+
 /**
  * In game console with output of error messages, feedback, and command inputs
  * for development (or cheating I guess)
@@ -16,28 +18,68 @@ export class Console {
     // the scene this console object should be attached to
     scene: Phaser.Scene;
 
+    // static HTML elements that are created once and re-used
+    // the <pre> output element which will contain console output
+    static outputEl: HTMLPreElement = null;
+    // the <div> which contains the output element, handles scrolling, and
+    // determines the consoles size and stlying in game
+    static containerEl: HTMLDivElement = null;
+    // the <style> which handles syntax highlighting of the console's output
+    static highlightEl: HTMLStyleElement = null;
+
     /**
      * Constructs a Console object
      * @param scene the scene this console object should be attached to
      */
     constructor(scene: Phaser.Scene) {
         this.scene = scene;
+        // Create the HTML elements forming the game console
+        this.create();
+        // Add the console to it's scene
+        this.scene.add.dom(px(100) - 150, py(100) - 75, Console.containerEl);
+    }
+
+    create() {
+        // Create the output container if it does not already exist
+        if (!Console.containerEl) {
+            Console.containerEl = document.createElement('div');
+            Console.containerEl.id = 'log-container';
+            Console.containerEl.style.overflow = 'auto';
+            Console.containerEl.style.width = '300px';
+            Console.containerEl.style.height = '150px';
+            Console.containerEl.style.backgroundColor = '#222';
+        }
+
+        // Create the output text element if it does not already exist
+        if (!Console.outputEl) {
+            Console.outputEl = document.createElement('pre');
+            Console.outputEl.id = 'log';
+            Console.containerEl.appendChild(Console.outputEl);
+        }
+
+        // Create style element that handles highlighting if it does not exist
+        if (!Console.highlightEl) {
+            Console.highlightEl = document.createElement('style');
+            Console.highlightEl.type = 'text/css';
+            Console.highlightEl.innerHTML = ".log-warn { color: orange } \
+            .log-error { color: red } \
+            .log-info { color: skyblue } \
+            .log-log { color: silver } \
+            .log-warn, .log-error { font-weight: bold; }";
+            Console.containerEl.appendChild(Console.highlightEl);
+        }
     }
 
     /**
      * Redirects the output of all major console output functions to
      * HTML game console as well as browser debug console.
-     * @param outputElement the <pre> output element which will contain console
-     * output
-     * @param outputContainerDiv the <div> which contains the output element,
-     * handles scrolling, and determines the consoles size and stlying in game 
      */
-    rewireAll(outputElement: HTMLPreElement, outputContainerDiv: HTMLDivElement) {
-        this.rewireConsoleFunc('log', outputElement, outputContainerDiv);
-        this.rewireConsoleFunc('debug', outputElement, outputContainerDiv);
-        this.rewireConsoleFunc('warn', outputElement, outputContainerDiv);
-        this.rewireConsoleFunc('error', outputElement, outputContainerDiv);
-        this.rewireConsoleFunc('info', outputElement, outputContainerDiv);
+    rewireAll() {
+        this.rewireConsoleFunc('log');
+        this.rewireConsoleFunc('debug');
+        this.rewireConsoleFunc('warn');
+        this.rewireConsoleFunc('error');
+        this.rewireConsoleFunc('info');
     }
 
     /**
@@ -45,12 +87,8 @@ export class Console {
      * browser console and some HTML elements which form the game's console
      * @param consoleFuncName the name of the console function to redirect to
      * HTML output
-     * @param outputElement the <pre> output element which will contain console
-     * output
-     * @param outputContainerDiv the <div> which contains the output element,
-     * handles scrolling, and determines the consoles size and stlying in game 
      */
-    rewireConsoleFunc(consoleFuncName: string, outputElement: HTMLPreElement, outputContainerDiv: HTMLDivElement) {
+    rewireConsoleFunc(consoleFuncName: string) {
         // store the old version of the console function
         console['_old_' + consoleFuncName] = console[consoleFuncName];
 
@@ -63,19 +101,19 @@ export class Console {
             // get HTML span tags of the objects
             const output = Console.htmlLog(consoleFuncName, args);
             // add the html <span> texts to the <pre> output element
-            outputElement.innerHTML += output + "<br>";
+            Console.outputEl.innerHTML += output + "<br>";
 
             // determine if game console is currently scrolled to the bottom
             const bottomed =
-                outputContainerDiv.scrollHeight
-                - outputContainerDiv.clientHeight
-                <= outputContainerDiv.scrollTop + 1;
+                Console.containerEl.scrollHeight
+                - Console.containerEl.clientHeight
+                <= Console.containerEl.scrollTop + 1;
             // automatically scroll to the bottom when new console output
             // is created
             if (!bottomed) {
-                outputContainerDiv.scrollTop =
-                    outputContainerDiv.scrollHeight
-                    - outputContainerDiv.clientHeight;
+                Console.containerEl.scrollTop =
+                    Console.containerEl.scrollHeight
+                    - Console.containerEl.clientHeight;
             }
 
             // run the old function too, to print to the console
@@ -83,7 +121,6 @@ export class Console {
         };
 
     };
-
 
     /**
      * Produces an HTML formatted string of tags containing logged objects
