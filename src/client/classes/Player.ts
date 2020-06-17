1e70896dfa7a20a93c94f82b0ff38978a010ce6c
Zachary <zachkingcade@gmail.com>
2020-06-17 12:01:31 -0500

    got depth and movement timeout working

--- src/client/classes/Player.ts
+++ src/client/classes/Player.ts
@@ -23,7 +23,10 @@ export class Player {
     /**The speed at which oue character move when in free roam */
     freeRoamSpeed: number;
     /**Used to allow the sprites of the non-leader party member to follow the leader */
-    path: {x: number, y: number}[];
+    path: {x: number, y: number, facing: string}[];
+    /**the depth at which we start when setting the depth for characters it goes from here to
+     * +4 of here assuming 4 party members */
+    startDepth: number;
 
     /**Boolean */
     /**Used to tell if the party change is in timeout */
@@ -51,14 +54,14 @@ export class Player {
         this.freeRoamSpeed = 130;
         this.leaderChangeTimeOut = false;
         this.path = [];
-        this.path[0] = {x: this.x, y: this.y};
+        this.path[0] = {x: this.x, y: this.y, facing: "down"};
+        this.startDepth = 10;
     }
 
     addPartyMemeberByKey(key: string){
-        /**construct our new party member and add them to the party */
+        //construct our new party member and add them to the party
         let newPartyMember = new Character(this.currentScene.anims);
         newPartyMember.createSprite(this.currentScene,key,this.x,this.y);
-        //newPartyMember.sprite.setScale(0);
         this.party.push(newPartyMember);
     }
 
@@ -69,29 +72,31 @@ export class Player {
     }
 
     changeLeader(){
-        /**set the timeout to be true to prevent rapid changing */
+        //set the timeout to be true to prevent rapid changing
         this.leaderChangeTimeOut = true;
-        /**removes the current leader from the front and add them to the back */
+        //grab the current leaders direction to keep consistancy
+        let direction = this.party[0].facingDirection;
+        //remove the current leader from the front and add them to the back
         this.x = this.party[0].sprite.x;
         this.y = this.party[0].sprite.y;
-        console.log(this.party[0].name, this.party[0].sprite.scale);
         this.party.push(this.party.shift())
-        /**now set the new leader */
+        //now set the new leader and make sure their facing the same direction
         this.party[0].moveTo(this.x,this.y);
-        /**fix the current scenes main camera to follow the new leader */
+        this.party[0].facingDirection = direction;
+        //fix the current scenes main camera to follow the new leader
         this.currentScene.cameras.main.startFollow(this.party[0].sprite,true);
         console.log(this.party[0].name, this.party[0].sprite.scale);
 
         setTimeout(() => {this.leaderChangeTimeOut = false;}, 500);
     }
 
-    /**Adds a new position to the path and check to make sure the path
+    /**Adds a new position to the path and checks to make sure the path
      * has not grown bigger then 120 elements */
-    addToPath(newX:number, newY:number){
+    addToPath(newX:number, newY:number, newFacing: string){
         if (Math.abs(newX - this.path[0].x) > 4 || Math.abs(newY - this.path[0].y) > 4){
-            let newlength = this.path.unshift({x:newX, y:newY});
+            let newlength = this.path.unshift({x:newX, y:newY, facing: newFacing});
             /*to help with performance we wait till it fill to 120 then splice off
-            everyhting back down to 80*/
+            everyhting back down to 80 */
             if(newlength > 120){
                 this.path.splice(80);
             }
@@ -101,15 +106,42 @@ export class Player {
     updatePartyOnPath(){
         for(let i = 1; i < this.party.length; i++){
             if (this.path.length > i * 3){
+                let body = <Phaser.Physics.Arcade.Body>this.party[i].sprite.body;
+                //determine movement for the non-leader party members
                 let rangeX = Math.abs(this.path[i*3].x - this.party[i].sprite.x);
                 let rangeY = Math.abs(this.path[i*3].y - this.party[i].sprite.y);
                 if (rangeX > 3 || rangeY > 3){
                     this.currentScene.physics.moveTo(this.party[i].sprite, this.path[i*3].x, this.path[i*3].y, this.freeRoamSpeed);
                 } else {
-                    let body = <Phaser.Physics.Arcade.Body>this.party[i].sprite.body;
                     body.setVelocity(0,0);
                 }
+                /*determine what sprite/animation to use if currently in motion use the walk animation in the direction
+                recorded by the path, else use the idle aninmation in that direction */
+                if (body.velocity.x || body.velocity.y){
+                    if (this.party[i].sprite.anims.getCurrentKey() != this.party[i].spriteKey + "walk_" + this.path[i*3].facing){
+                        this.currentScene.anims.play(this.party[i].spriteKey + "walk_" + this.path[i*3].facing, this.party[i].sprite);
+                    }
+                } else {
+                    if (this.party[i].sprite.anims.getCurrentKey() != this.party[i].spriteKey + "idle_" + this.path[i*3].facing){
+                        this.currentScene.anims.play(this.party[i].spriteKey + "idle_" + this.path[i*3].facing, this.party[i].sprite);
+                    }
+                }
             }
+        }
+    }
+
+    updateDepth(){
+        //declares the format of our array and instantiatesd it
+        let heightArray: {index: number, y: number}[] = [];
+        //first grab all the depths of the party members
+        for (let i = 0; i < this.party.length; i++){
+            heightArray.push({index: i, y: this.party[i].sprite.y})
+        }
+        //sort the array by y value
+        heightArray.sort((a,b) => {return a.y - b.y});
+        //finally set their depth based on the sorted array
+        for (let i = 0; i < heightArray.length; i++){
+            this.party[heightArray[i].index].sprite.depth = this.startDepth + i;
         }
     }
 
@@ -117,7 +149,7 @@ export class Player {
      * runs any methods that need to be ran accordingly.
      */
     updatePlayerInput(){
-        /**first we'll check for key pressed related to movement and move the leader based on those */
+        //first we'll check for key pressed related to movement and move the leader based on those
         let x = 0;
         let y = 0;
         if (this.controls.isDown("walk up")){
@@ -132,10 +164,15 @@ export class Player {
         if (this.controls.isDown("walk right")){
             x += this.freeRoamSpeed
         }
+        if (this.leaderChangeTimeOut){
+            x = 0;
+            y = 0;
+        }
         this.party[0].UpdateMovement(x,y);
-        this.addToPath(this.party[0].sprite.x, this.party[0].sprite.y);
+        this.addToPath(this.party[0].sprite.x, this.party[0].sprite.y, this.party[0].facingDirection);
         this.updatePartyOnPath();
-        /**check for input to change the party leader */
+        this.updateDepth();
+        //check for input to change the party leader
         if (this.controls.isDown("change leader") && !this.leaderChangeTimeOut){
             this.changeLeader();
         }
