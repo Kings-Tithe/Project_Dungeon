6e2a5a7fd7863f9864ee7e378cbf8f0bca02b18e
Jeremy Glebe <jeremyglebe@gmail.com>
2020-06-23 11:23:28 -0500

    fixed addPortrait not being received

--- src/client/scenes/overlays/Hud.ts
+++ src/client/scenes/overlays/Hud.ts
@@ -14,14 +14,14 @@ export class Hud extends Phaser.Scene {
      * so in the form of an array of objects where the objects hold the spritekey
      * and the sprite it's self, the spritekey is stored so we can refrence that
      * sprite more easily */
-    portraitSprites: {[key: string]: Phaser.GameObjects.Sprite};
+    portraitSprites: { [key: string]: Phaser.GameObjects.Sprite };
     /**Sprite frame at the top of the screen that other ui elements hit ontop of it
      * sets a depth of 5. The character portrait sits under it */
     characterFrame: Phaser.GameObjects.Sprite;
 
     //strings
     /**Stores the spritekey of the currently in use character portrait */
-    currentPortrait: string; 
+    currentPortrait: string;
 
     //GlobalEmitter
     /**Stores a refernce to the global event emitter */
@@ -41,21 +41,21 @@ export class Hud extends Phaser.Scene {
         con.rewireAll();
 
         //create character frame
-        this.characterFrame = this.add.sprite(0,0,"characterFrame").setOrigin(0,0);
+        this.characterFrame = this.add.sprite(0, 0, "characterFrame").setOrigin(0, 0);
         this.characterFrame.setScale(2);
         this.characterFrame.setDepth(1);
 
         //events to listen for
-        this.globalEmitter.on("addPortrait", console.log);
+        this.globalEmitter.on("addPortrait", (portrait) => { console.log("Portrait Data: ", portrait) });
         this.globalEmitter.on("changePortrait", this.changePortraitSprite, this);
     }
 
     /**Used to add to the list of character portrait sprites we have in portraitSprites */
-    addPortraitSprite(spritekey: string){
-        setTimeout(() => {console.log('test01');}, 2000); 
-        if (this.portraitSprites[spritekey] == null){
+    addPortraitSprite(spritekey: string) {
+        setTimeout(() => { console.log('test01'); }, 2000);
+        if (this.portraitSprites[spritekey] == null) {
             //create the new sprite if it is not already in the list
-            let newPortrait = this.add.sprite(10,12,spritekey).setOrigin(0,0);
+            let newPortrait = this.add.sprite(10, 12, spritekey).setOrigin(0, 0);
             newPortrait.setScale(1.3);
             newPortrait.setDepth(0);
             //add to list
@@ -65,17 +65,17 @@ export class Hud extends Phaser.Scene {
         }
 
         //if there is no sprite currently in use, use the new one
-        if (typeof this.currentPortrait !== 'undefined'){
+        if (typeof this.currentPortrait !== 'undefined') {
             this.currentPortrait = spritekey;
             this.portraitSprites[this.currentPortrait].setScale(1.3);
         }
     }
 
     /**Used to change what portrait is currently being shown */
-    changePortraitSprite(spritekey){
+    changePortraitSprite(spritekey) {
         console.log(this.portraitSprites);
         //if the sprite exists hide the current and scale to visible the passed in portrait
-        if (this.portraitSprites[spritekey] != null){
+        if (this.portraitSprites[spritekey] != null) {
             this.portraitSprites[this.currentPortrait].setScale(0);
             this.portraitSprites[spritekey].setScale(1.3);
         } else {
